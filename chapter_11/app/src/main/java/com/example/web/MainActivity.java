e31b934f4d3e9881916571306a603e54f6ebd41f
json4256 <json4256@uni.sydney.edu.au>
2020-09-27 10:58:12 +1000

    pass

---
+++ chapter_11/app/src/main/java/com/example/web/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.web;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.net.http.SslError;
+import android.os.Bundle;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                //等待证书响应
+                handler.proceed();
+            }
+        });
+    }
+
+    private void init(){
+        webView = (WebView) findViewById(R.id.webView);
+        //需要加载的网页的URL
+        webView.loadUrl("https://www.google.com");
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient(){
+            public boolean shouldOverrideUrlLoading(WebView view, String url){
+                view.loadUrl(url);
+                return true;
+            }
+        });}}
